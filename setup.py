5eb717a52ce55df3f2792625273164efa5dafbb9
Vương Gia Huy <vuonggiahuy@192.168.1.11>
2025-05-24 22:23:55 +0700

    Modified description and added long description for the project

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ from app import __version__
 
 setup(
     name="mlx-openai-server",
-    version=__version__,
-    description="OpenAI-compatible server for MLX models",
+    url="https://github.com/cubist38/mlx-openai-server",
     author="Huy Vuong",
+    author_email="cubist38@gmail.com",
+    version=__version__,
+    description="A high-performance API server that provides OpenAI-compatible endpoints for MLX models. Built with Python and FastAPI, it enables efficient, scalable, and user-friendly local deployment of MLX-based vision and language models with an OpenAI-compatible interface. Perfect for developers looking to run MLX models locally while maintaining compatibility with existing OpenAI-based applications.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(), 
     install_requires=[
         "mlx-vlm==0.1.26",
