770e05e1ed9d687379e16a2d9682cda8d2291db3
Vương Gia Huy <vuonggiahuy@MacBook-Pro-cua-Gia-Huy.local>
2025-07-03 13:31:50 +0700

    sync latest code `mlx_vlm` for running gemma-3n-e4b vision

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(), 
     install_requires=[
-        "mlx-vlm==0.2.0",
+        "git+https://github.com/Blaizzy/mlx-vlm.git",
         "mlx-lm==0.25.3",
         "fastapi",
         "uvicorn",
