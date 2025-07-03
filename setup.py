76f6af9121f72992e722e50615ef227d69defa65
Vương Gia Huy <vuonggiahuy@MacBook-Pro-cua-Gia-Huy.local>
2025-07-03 13:58:59 +0700

    mlx-vlm should be latest commit version for working with

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(), 
     install_requires=[
-        "mlx-vlm==0.2.0",
+        "mlx-vlm @ git+https://github.com/Blaizzy/mlx-vlm.git@ebafa5a789ed1a8e050b8366ae4e845dbe640b90",
         "mlx-lm==0.25.3",
         "fastapi",
         "uvicorn",
