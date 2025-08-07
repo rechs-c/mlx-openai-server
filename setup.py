86d197d97f9adb8ca0325a2816eb1a9f709908e0
Vương Gia Huy <vuonggiahuy@MacBook-Pro-cua-Gia-Huy.local>
2025-08-07 09:43:16 +0700

    update latest `mlx-lm` for supporting `gpt-oss` series

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(), 
     install_requires=[
         "mlx-vlm==0.3.2",
-        "mlx-lm==0.26.2",
+        "mlx-lm==0.26.3",
         "mlx-embeddings==0.0.3",
         "mflux==0.9.3",
         "fastapi==0.115.14",
@@ -23,6 +23,7 @@ setup(
         "click==8.2.1",
         "loguru==0.7.3",
         "outlines==1.1.0",
+        "openai-harmony==0.0.3"
     ],
     extras_require={
         "dev": [
