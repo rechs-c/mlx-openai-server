8bae6f07c3a2858a1eba61a9ef6331eb5e813572
Vương Gia Huy <vuonggiahuy@192.168.1.3>
2025-08-07 22:54:48 +0700

    applied `HarmonyParser` to `gpt-oss` series for server side

--- app/models/mlx_lm.py
+++ app/models/mlx_lm.py
@@ -180,7 +180,7 @@ class MLX_LM:
         input_tokens = self.tokenizer.apply_chat_template(
             messages,
             add_generation_prompt=True,
-            **chat_template_kwargs
+            **chat_template_kwargs,
         )
         
         sampler = make_sampler(
