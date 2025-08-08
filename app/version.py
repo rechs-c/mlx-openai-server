cfabb5eb2f7eba98e388d164074a67fda9875f48
Vương Gia Huy <vuonggiahuy@MacBook-Pro-cua-Gia-Huy.local>
2025-08-08 11:52:38 +0700

    bump to version 1.2.5

--- app/version.py
+++ app/version.py
@@ -1,1 +1,6 @@
+# Version number format: MAJOR.MINOR.PATCH
+# Major: Major version number (increments when breaking changes are introduced)
+# Minor: Minor version number (increments when new features are added)
+# Patch: Patch version number (increments when bug fixes are made)
+
 __version__ = "1.2.5"   
